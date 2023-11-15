4dfb7eb9f873f6e417fad6a90cfeba259d260361
thomas mariotte <thomasmariotte@hotmail.fr>
2023-11-15 15:51:06 +0100

    Pushing front for Nicolas

---
+++ project-cloud/src/frontend/Notes.tsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+import React from 'react';
+
+function Notes() {
+
+  const [notes, setNotes] = useState([]);
+
+
+  return (
+    <div>
+      <h1>Mes notes</h1>
+    </div>
+  );
+}
+
+export default Notes;
