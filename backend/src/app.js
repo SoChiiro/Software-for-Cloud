6e859cc116202bdac426ce726e8e62de29d8bdc1
Thomas Mariotte <thomas.mariotte@efrei.net>
2024-01-19 11:06:41 +0100

    Update

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,15 +25,10 @@ async function getUsers() {
   }
 }
 
-
 async function startServer() {
   try {
     await dbConnect();
     console.log(".... Server Connected to the database !");
-
-    // const users = await getUsers();
-    // console.log('Users:', users);
-
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
     });
@@ -43,29 +38,3 @@ async function startServer() {
 }
 
 startServer();
-
-// const express = require('express');
-// const app = express()
-// const router = express.Router();
-// const mongoose = require('mongoose');
-
-// // const port = 3001;
-// app.use(express.json());
-
-// const port = process.env.PORT || 3006
-// const dbUrl = 'mongodb+srv://nolyuks:0hYGP4JOWCGWqkxO@vote.zwr4n6b.mongodb.net/';
-
-// // Connexion à la base de données MongoDB
-// mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     console.log('Connecté à la base de données MongoDB réussi');
-//   })
-//   .catch((error) => {
-//     console.error('Erreur de connexion à la base de données :', error);
-//   });
-
-// app.listen(port, () => {
-//   console.log(`Serveur démarré (http://localhost:${port}/) !`);
-// });
-
-// module.exports = router;
